b41c448a74853f091e0387b6e298d0d772ba5e38
linhui <linhui@1cfun.com>
2023-03-26 12:53:41 +0800

    1

--- app/Controller/CalculatorController.php
+++ app/Controller/CalculatorController.php
@@ -54,6 +54,7 @@ class CalculatorController extends AbstractController
  * root  root
  * ip address
  * 在项目根目录   php bin/hyperf.php start 或  nohup php bin/hyperf.php start &
+ *  netstat -anp | grep 9501
  * 本机 curl 127.0.0.1:9501   宿主 nginx  192.168.1.109:80
  * CTRL + C 终止
  *
@@ -71,4 +72,27 @@ class CalculatorController extends AbstractController
 }
  *
  * curl -X POST -H "Content-Type: application/json" --data "{\"jsonrpc\": \"2.0\", \"method\": \"/Calculator/add\", \"params\":[], \"id\":1,\"context\": []}" http://127.0.0.1:9504
+ *
+ * jsonrpc 错误码：http://www.caotama.com/2136386.html
+ *
+ *
+ * consul -v
+ * 以开发模式启动，k/v临时存储，重启后将丢失
+ * nohup consul agent -dev -ui -node=consul-dev -client=0.0.0.0 &
+ * nohup consul agent -dev -ui -node=consul-dev -client=192.168.1.109 &
+ * nohup consul agent -dev -ui -node=consul-dev -client=127.0.0.1 &
+ * 重要：  client ip决定者访问地址，否则拒绝连接
+ *
+ *
+ * 暂停
+    pid=$(ps aux | grep consul | awk 'NR==1'|awk '{print $2}')
+    kill -9 $pid
+ * 正式启动
+ * nohup ./consul agent -server -ui -bootstrap-expect=1 -data-dir=/tmp/consul -node=agent-one -bind=x.x.x.x -client=x.x.x.x &
+ *
+ *查看服务器开放的端口 netstat -tuln
+ * 防火墙端口是否开放 https://blog.csdn.net/sl807826634/article/details/128145611
+ * netstat -anp | grep 8500
+ * curl http://192.168.1.109:8500/ui/dc1/services
  */
+
--- config/autoload/dependencies.php
+++ config/autoload/dependencies.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  */
 return [//注解依赖注入
     'InnerHttp' => Hyperf\HttpServer\Server::class,
-    //App\JsonRpcClient\CalculatorServiceInterface::class => App\JsonRpc\CalculatorService::class,
+    App\JsonRpc\CalculatorServiceInterface::class => App\JsonRpc\CalculatorService::class,
     App\JsonRpcClient\CalculatorServiceInterface::class => App\JsonRpcClient\CalculatorServiceConsumer::class,
     //App\JsonRpc\CalculatorServiceInterface::class => App\JsonRpc\CalculatorServiceConsumer::class,//支撑消费者类，这样便可以通过注解依赖注入的方式 注入 CalculatorServiceInterface 接口来使用客户端了。
 ];
